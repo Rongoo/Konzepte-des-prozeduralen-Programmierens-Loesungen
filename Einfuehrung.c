98379b9e4e1ab67134fe0c111bc90a00d42cc2f2
ge72caw <paulgeraldneuner@gmail.com>
2021-05-17 20:09:51 +0200

    revertet to origninal state

--- Einfuehrung.c
+++ Einfuehrung.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int werte[2];
-    werte[0]=1;
-    werte[1]=0;
-    int i = 0;
-    printf("%d",werte[i++]);
-    /*
+
 int number,anotherNumber;
 printf("I want to know you number :");
 scanf("%d , %d",&number,&anotherNumber);
@@ -15,7 +10,7 @@ printf("Bip-.Bup-.Bap-. Your numbers are: %d , %d",number, anotherNumber);
 }else{
     printf("Bip-.Bup-.Bap-. Your numbers are too small I don't want to print them");
 }
-*/
+
 return 0;
 }
 
