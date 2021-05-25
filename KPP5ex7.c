edd7e4da02cd153ed31a15a5026b2c43e8d1b309
Rongo <paulgeraldneuner@gmail.com>
2021-05-25 18:15:42 +0200

    added chomp for visual correctness

--- KPP5ex7.c
+++ KPP5ex7.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
 #define LEN 1000
 
+void chomp(char text[]){
+    int i;
+    for(i = 0; text[i];i++);
+    if(text[i-1]=='\n'){
+        text[i-1]=0;
+    }
+}
 
 int main(){
 char text[LEN];
 printf("Bitte geben sie Ihren Text ein!\n");
 fgets(text, LEN , stdin);
+chomp(text);
 for(int i = 0; text[i];i++){
     if(text[i] == 'e'){
         int j;
