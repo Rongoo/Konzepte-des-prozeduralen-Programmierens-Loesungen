c2c77fb0db59d11db2c32d2842d9b83a21e00ec8
Rongoo <paulgeraldneuner@gmail.com>
2021-05-27 10:24:00 +0200

    KPP6ex1.c added and completed

---
+++ KPP6ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define LEN 100
+
+int main(){
+    char text[LEN];
+    printf("Bitte geben sie einen Text ein!");
+    fgets(text,LEN,stdin);
+    int string_len;
+    for(string_len = 0; *(text+string_len);string_len++);
+    string_len--;
+    for(int i = 0; i<string_len/2;i++){
+        if(*(text+i)!= *(text+string_len-(i+1))){
+            printf("Kein Palimdrom!");
+            return 0;
+        }
+    }
+    printf("Palimdrom!");
+    return 0;
+}
